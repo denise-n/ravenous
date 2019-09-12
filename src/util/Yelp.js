3b7836ea0cca8017d03a30360c1d5e9dd26c6685
Denise <denise.ndalamba@gmail.com>
2019-09-12 17:48:57 -0400

    added Yelp.js to fetch data

---
+++ src/util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = '7SRpAAVFhCtn9_enL5O8rXLr1JpYMSVaL6SevnEQvJSWDnaaPd8I3sko7MR2Rfe7oQ8p33u9o4BgGf9rmWSF5Q8pB2bZHCek7XgGPkeECgdM2oMrPI9W2kBXuqd6XXYx'
+const Yelp = {}
+const search = (term, location, sortBy) => {
+    return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}', {
+        headers: {
+            'Authorization' : `Bearer ${apiKey}`
+        }
+    }).then(response => {
+        return response.json()
+    }).then(jsonResponse => {
+        if(jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.reviewCount
+                }
+            })
+        }
+    })
+
+}
